554cca6ac00394064ddb135229f485ec20c2ade4
socbaycff <socbaycff@gmail.com>
2019-12-24 12:17:19 +0700

    update datachinh

--- app/src/main/java/com/example/github/MainActivity.java
+++ app/src/main/java/com/example/github/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(),"Hello before",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"Hello before2",Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "Hello database", Toast.LENGTH_LONG).show();
     }
 }
