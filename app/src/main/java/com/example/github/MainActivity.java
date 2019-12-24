500b13fbe4d1cb2a5825c6960c983cd88a6bc16a
socbaycff <socbaycff@gmail.com>
2019-12-24 12:13:22 +0700

    save before data

--- app/src/main/java/com/example/github/MainActivity.java
+++ app/src/main/java/com/example/github/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"Hello before",Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(), "Hello database", Toast.LENGTH_LONG).show();
     }
 }
